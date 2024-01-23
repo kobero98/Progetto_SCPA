2a3240d07b5fc0b6d3e70772dbe1a5c2ed69e441
Fanfarillo <matteo.fanfarillo99@gmail.com>
2024-01-23 14:59:50 +0100

    Little fix on Makefile and start.sh

--- mpi/start.sh
+++ mpi/start.sh
@@ -3,55 +3,59 @@ mkdir target
 mkdir result
 
 module load mpi/openmpi-x86_64;
-mpicc MPI-matrix-gen.c -o target/MPI-matrix-gen
-mpicc MPI-prod-kobero.c -o target/MPI-prod-kobero
+mpicc MPI-matrix-gen.c -o target/MPI-matrix-gen -O3
+mpicc MPI-prod-$1.c -o target/MPI-prod-$1 -O3
 cd target
 
 seed=123456789
 for K in {10,50,100,1000,2000,3000,4000}
 do 
-    dir="matrix_Seed_$seed-$K"
+    dir="matrix-seed-$seed-$K"
     echo ciao $dir $seed
     mkdir $dir
     mpirun -n 3 MPI-matrix-gen $seed $dir $K $K $K
-    mpirun -n 1 MPI-prod-kobero $dir > ../result/result-kobero-$K-1
+    mpirun -n 1 MPI-prod-$1 $dir > ../result/result-$1-$K-1
 
     for p in {5,10,15,20}
     do
-        echo "[" > ../result/result-kobero-$K-$p
+        echo "[" > ../result/result-$1-$K-$p
 
         for n in {1..19}
         do
             echo "dimension problema: $K numero processi: $p ";
-            echo "{\"iterazione\":$n,\"vettore\":[" >> ../result/result-kobero-$K-$p
-            mpirun -n $p MPI-prod-kobero $dir >> ../result/result-kobero-$K-$p
-            echo "{}]}," >> ../result/result-kobero-$K-$p
+            echo "{\"iterazione\":$n,\"vettore\":[" >> ../result/result-$1-$K-$p
+            mpirun -n $p MPI-prod-$1 $dir >> ../result/result-$1-$K-$p
+            echo "{}]}," >> ../result/result-$1-$K-$p
         done
-        echo "{\"iterazione\":20,\"vettore\":[" >> ../result/result-kobero-$K-$p
-        mpirun -n $p MPI-prod-kobero $dir >> ../result/result-kobero-$K-$p
-        echo "{}]}]" >> ../result/result-kobero-$K-$p
+        echo "{\"iterazione\":20,\"vettore\":[" >> ../result/result-$1-$K-$p
+        mpirun -n $p MPI-prod-$1 $dir >> ../result/result-$1-$K-$p
+        echo "{}]}]" >> ../result/result-$1-$K-$p
 
     done
 done
+
 echo 
 for K in {5000,8000,10000,11000,12000,15000}
 do
-    dir="matrix_Seed_$seed-$K"
+    dir="matrix-seed-$seed-$K"
     echo ciao $dir $seed
     mkdir $dir
-    mpirun -n 3 mgmpi $seed $dir $K $K $K
+    mpirun -n 3 MPI-matrix-gen $seed $dir $K $K $K
+
     for p in {4,8,16,20}
     do
-        echo "[" > ../result/result_Kobero_$K-$p
+        echo "[" > ../result/result-$1-$K-$p
+
         for n in {1..19}
         do
             echo "dimension problema: $K numero processi: $p ";
-            echo "{\"iterazione\":$n,\"vettore\":[" >> ../result/result_Kobero_$K-$p
-            mpirun -n $p MPI-prod-kobero $dir >> ../result/result_Kobero_$K-$p
-            echo "{}]}," >> ../result/result_Kobero_$K-$p
+            echo "{\"iterazione\":$n,\"vettore\":[" >> ../result/result-$1-$K-$p
+            mpirun -n $p MPI-prod-$1 $dir >> ../result/result-$1-$K-$p
+            echo "{}]}," >> ../result/result-$1-$K-$p
         done
-        echo "{\"iterazione\":20,\"vettore\":[" >> ../result/result_Kobero_$K-$p
-        mpirun -n $p MPI-prod-kobero $dir >> ../result/result_Kobero_$K-$p
-        echo "{}]}]" >> ../result/result_Kobero_$K-$p
+        echo "{\"iterazione\":20,\"vettore\":[" >> ../result/result-$1-$K-$p
+        mpirun -n $p MPI-prod-$1 $dir >> ../result/result-$1-$K-$p
+        echo "{}]}]" >> ../result/result-$1-$K-$p
+
     done
 done
